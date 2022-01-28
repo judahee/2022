5c7fb278478c184bfb4089da6c0760d2ba30ece1
dahee <82073087+judahee@users.noreply.github.com>
2022-01-28 22:27:24 +0900

    Update 0128.py

--- level_1/0128.py
+++ level_1/0128.py
@@ -8,15 +8,14 @@
 
 첫째 줄에는 A × B × C의 결과에 0 이 몇 번 쓰였는지 출력한다. 마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다
 '''
+import sys
 
 A = int(input())
 B = int(input())
 C = int(input())
 
-cal = A*B*C
-ans = []
+cal = list(str(A*B*C))
 
-while True :
-  if (cal<=0):
-    break
-  
+for i in range(10):
+    print(cal.count(str(i)))
+
